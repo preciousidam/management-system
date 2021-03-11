c984012e2cbc7554b20b00fabafd24f3f5752ba8
preciousidam <preciousidam@gmail.com>
2021-03-11 21:09:11 +0100

    ...

--- core/settings.py
+++ core/settings.py
@@ -173,7 +173,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
